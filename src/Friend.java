ea87974e87d972cf7f7c210ea354bd4165823f56
ChunXela <Johansson.axel@protonmail.com>
2022-09-27 15:46:00 +0200

    First commit

---
+++ src/Friend.java
@@ -0,0 +1,12 @@
+public class Friend {
+
+    String name = "A friend";
+    int hp = 100;
+    boolean isFrined = true;
+
+    public Friend(){}
+
+    public Friend (String name){
+        this.name    = name;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+
+        Friend aFriend = new Friend();
+        Friend myFriend = new Friend("Karl");
+    }
+
+}
